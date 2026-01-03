edebf58aea08ebc96d9d5b0a80dd96f2818f08f3
Your Name <you@example.com>
2026-01-03 06:39:03 +0530

    register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { BaggageClaim } from 'lucide-react';
 import {API} from '../api';
-
+import axios from 'axios';
 
 function Register() {
   const [form, setForm] = useState({
@@ -19,6 +18,11 @@ function Register() {
     setForm({...form, [e.target.name]: e.target.value});
   }
 
+  consthandleRegisters = async(e) => {
+    e.preventDefault();
+    await axios.post(`${API}/auth/register`, form);
+    navigate("/");
+  };
 
   return (
      <div className="row">
@@ -29,11 +33,7 @@ function Register() {
           <p className='text-5xl font-bold text-center mt-5 mb-50 '>Welcome to ...</p>
       <p  className='text-xl font-bold text-center items-center mb-50 '> Delivering the essentials straight to your home</p>
       <h2 className='text-xl font-bold mb-4 col-8 ml-250'>Register Form</h2>
-      <form onSubmit={async (e) => {
-            e.preventDefault();
-            await axios.post(`${API}/auth/register`, form);
-            navigate("/");
-          }} className='space-y-4 col-12 ml-250'>
+      <form onSubmit={handleRegisters} className='space-y-4 col-12 ml-250'>
         <input onChange={handleChange} name='username' className='border p-2 mb-2 w-full rounded-md' type="text" placeholder='John' />
         <input onChange={handleChange} name='email' className='border p-2 w-full  mb-2 rounded-md' type="email" placeholder='john@mail.com' />
         <input onChange={handleChange} name='password' className='border p-2 w-full mb-2  rounded-md' type="password" placeholder='*****' />
